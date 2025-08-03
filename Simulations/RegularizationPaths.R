871e20f84f80817df79e81665d5cec984d7ecde8
JeroenGoedhart <jeroengoed@gmail.com>
2025-08-03 13:41:08 +0200

    update

--- Simulations/RegularizationPaths.R
+++ Simulations/RegularizationPaths.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/VNOB-0732/Desktop/R files/ClinOmics/Simulations")
+setwd("~/PhD files/R files/ClinOmics/FusedTree_Paper/Simulations")
 source('SideFunctions_Update.R')
 source('RidgeFunctions_Update.R')
 library(rpart); 
@@ -26,7 +26,7 @@ betas <- matrix(rnorm(p,0,5/p),ncol = 1)
 betas
 Varids <- c(2,6)
 betas[Varids]
-betas
+
 g <- function (z,x,betas){
   1*(z[,1] < 0.5)*(z[,2] < 0.5)*(-10 + x[,1:p/5] %*% betas[1:p/5]*6) + 
     1*(z[,1] < 0.5)*(z[,2] >= 0.5)*(-5 + x[,1:p/5]%*% betas[1:p/5]*3) +
@@ -74,12 +74,12 @@ beg <- NumNod * (Varids) + 1; end <- NumNod * (Varids) + 1 + 3
 ids1 <- c(seq(beg[1], end[1]), seq(beg[2], end[2]))
 
 
-alpha <- seq(0.001, 5000, 0.5)
+alpha <- seq(0.001, 5000, 1)
 
-lambda <- c(0, 1, 10, 100, 500, 5000)
+lambda <- c(0.0, 1, 10, 100, 500, 5000)
 results <- c()
 for (i in 1:length(lambda)) {
-  Lam <- lambda[i]
+  Lam <- lambda[i]+0.01
   Betas <- lapply(alpha, function (x) ridgeGLM2(Y = Y1, U = U1, X = as.matrix(X1), 
                                                lambda = Lam, lambdaG = x, Dg = as.matrix(Dg), 
                                                model = "linear"))
@@ -92,21 +92,55 @@ for (i in 1:length(lambda)) {
   colnames(Betas)[1] <- "c6"
   Res <- cbind.data.frame("Alpha" = log(alpha), Betas)
   melt <- melt(Res, id = "Alpha")
-  melt <- cbind.data.frame(melt,"Lambda" = rep(paste("Lambda = ",Lam), nrow(melt)))
+  melt <- cbind.data.frame(melt,"Lambda" = rep(paste("Lambda = ",Lam-0.01), nrow(melt)))
   results <- rbind.data.frame(results, melt)
 }
 
 unique(results$Lambda)
+results$Lambda_f <- factor(results$Lambda)
+
 results$Lambda_f <- factor(results$Lambda, levels = c("Lambda =  0","Lambda =  1","Lambda =  10",
-                                                   "Lambda =  100","Lambda =  500","Lambda =  5000"),ordered = T)
+                                                   "Lambda =  100","Lambda =  500","Lambda =  5000"), 
+                           ordered = T)
 results$Lambda_f <- gsub("Lambda", "\u03bb", results$Lambda_f) 
+names(effect_labels)
+effect_labels <- c(
+  "Effect_clin" = expression(c[6]),
+  "Effect_2,4" = expression(beta[2(N4)]),
+  "Effect_2,5" = expression(beta[2(N5)]),
+  "Effect_2,6" = expression(beta[2(N6)]),
+  "Effect_2,7" = expression(beta[2(N7)]),
+  "Effect_6,4" = expression(beta[6(N4)]),
+  "Effect_6,5" = expression(beta[6(N5)]),
+  "Effect_6,6" = expression(beta[6(N6)]),
+  "Effect_6,7" = expression(beta[6(N7)])
+)
+levels(results$variable) <- names(effect_labels)
 name <- paste("RegPath", "EffMod", sep = "_")
 library(Cairo)
 CairoPDF(name, width = 7, height = 7.5)
-plot <- ggplot(results, aes(x = Alpha, y = value, group = variable, colour = variable,linetype = variable)) + 
-  geom_line() + theme_light() +
-  scale_color_viridis(discrete = T, option = "D") + scale_linetype_manual(values = c("dashed",rep("solid",4),rep("dotted",4))) +
-  theme(legend.title = element_blank()) + labs(x = "log(\u03b1)", y = "Effect estimates") 
-plot + facet_wrap(. ~ Lambda_f, nrow=3)
+plot <- ggplot(results, aes(x = Alpha, y = value, group = variable, colour = variable, linetype = variable)) + 
+  geom_line() + 
+  theme_light() +
+  scale_color_viridis(
+    discrete = TRUE,
+    option = "D",
+    labels = effect_labels
+  ) +
+  scale_linetype_manual(
+    values = c("dashed", rep("solid", 4), rep("dotted", 4)),
+    labels = effect_labels
+  ) +
+  theme(
+    legend.title = element_blank(),
+    legend.text = element_text(size = 10)
+  ) +
+  labs(
+    x = expression(log(alpha)),
+    y = "Effect estimates"
+  )
+
+# Add facet
+plot + facet_wrap(. ~ Lambda_f, nrow = 3)
 dev.off()
 getwd()
